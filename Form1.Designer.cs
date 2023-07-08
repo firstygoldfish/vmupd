16f8a725ebb97384f73ffa405d1b2aff9774f88f
Carl <carl@capita.com>
2023-07-08 23:08:10 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace vmupd
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.butVMUpdate = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.butHelp = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(12, 65);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(776, 429);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // butVMUpdate
+            // 
+            this.butVMUpdate.Image = ((System.Drawing.Image)(resources.GetObject("butVMUpdate.Image")));
+            this.butVMUpdate.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.butVMUpdate.Location = new System.Drawing.Point(12, 11);
+            this.butVMUpdate.Name = "butVMUpdate";
+            this.butVMUpdate.Size = new System.Drawing.Size(150, 48);
+            this.butVMUpdate.TabIndex = 1;
+            this.butVMUpdate.Text = "Update VM";
+            this.butVMUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.butVMUpdate.UseVisualStyleBackColor = true;
+            this.butVMUpdate.Click += new System.EventHandler(this.butVMUpdate_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 497);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 32);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(60, 25);
+            this.lblStatus.Text = "Ready";
+            // 
+            // butHelp
+            // 
+            this.butHelp.Image = ((System.Drawing.Image)(resources.GetObject("butHelp.Image")));
+            this.butHelp.Location = new System.Drawing.Point(168, 11);
+            this.butHelp.Name = "butHelp";
+            this.butHelp.Size = new System.Drawing.Size(48, 48);
+            this.butHelp.TabIndex = 3;
+            this.butHelp.UseVisualStyleBackColor = true;
+            this.butHelp.Click += new System.EventHandler(this.butHelp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 529);
+            this.Controls.Add(this.butHelp);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.butVMUpdate);
+            this.Controls.Add(this.txtOutput);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VMUpd - Capita : OASys - Carl Last 2023";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button butVMUpdate;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button butHelp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using Renci.SshNet;
+
+namespace vmupd
+{
+    public partial class Form1 : Form
+    {
+		String srvip = "192.168.56.21";
+		String srvuser = "oracle";
+		String srvpswd = "delta1";
+		string NL = Environment.NewLine;
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+		}
+
+        private void butVMUpdate_Click(object sender, EventArgs e)
+        {
+			var confirmResult = MessageBox.Show("Ensure your VM is started and you are connected to the Azure VPN before continuing.  Once the process has started it should NOT be interrupted or data loss will occur." + NL + NL + "Are you sure to update your VM?",
+									 "Confirm Update!!",
+									 MessageBoxButtons.YesNo);
+			if (confirmResult == DialogResult.Yes)
+			{
+				txtOutput.Clear();
+				butVMUpdate.Enabled = false;
+				try
+				{
+					var ssh = new SshClient(srvip, srvuser, srvpswd);
+					lblStatus.Text = "Running...";
+					statusStrip1.BackColor = ColorTranslator.FromHtml("#00cc6a");
+					addtext("Connecting securely to VM...");
+					ssh.Connect();
+					var result = ssh.RunCommand("echo connected to $HOSTNAME" + NL);
+					addtext(result.Result);
+					result = ssh.RunCommand("rm -f ~/install.sh; sshpass -p \"delta1\" scp  -o ConnectTimeout=10 oracle@10.0.1.14:/backups/vmupdate/install.sh ~/");
+					if (result.Error.Length > 0)
+					{
+						errtext(result.Error);
+						resetForm(ssh);
+					}
+					else
+					{
+						addtext("Running upgrade script...");
+						result = ssh.RunCommand("chmod 755 ~/install.sh");
+						if (result.Error.Length > 0)
+						{
+							addtext(result.Error);;
+							resetForm(ssh);
+						}
+						else
+						{
+							var cmd = ssh.CreateCommand("~/install.sh 2>&1");
+							var cmdresult = cmd.BeginExecute();
+
+							using (var reader = new StreamReader(cmd.OutputStream, Encoding.UTF8, true, 1024))
+							{
+								while (!cmdresult.IsCompleted || !reader.EndOfStream)
+								{
+									string line = reader.ReadLine();
+									if (line != null)
+									{
+										addtext(line);
+										Application.DoEvents();
+									}
+									Application.DoEvents();
+								}
+							}
+
+							//cmd.EndExecute(cmdresult);
+
+							resetForm(ssh);
+						}
+					}
+				}
+				catch (System.Net.Sockets.SocketException)
+				{
+					errtext("Connection failed.");
+					resetForm(null);
+				}
+				catch (Renci.SshNet.Common.SshAuthenticationException)
+				{
+					errtext("Authentication failed.");
+					resetForm(null);
+				}
+			}
+		}
+
+		private void addtext(String text)
+        {
+			txtOutput.AppendText(text);
+			txtOutput.AppendText(NL);
+        }
+
+		private void errtext(String text)
+        {
+			addtext("ERROR: " + text);
+			addtext("Update terminated.");
+		}
+
+		private void resetForm(SshClient ssh)
+        {
+			butVMUpdate.Enabled = true;
+			lblStatus.Text = "Ready";
+			statusStrip1.BackColor = Form1.DefaultBackColor;
+			if (ssh != null && ssh.IsConnected) ssh.Disconnect();
+		}
+
+        private void butHelp_Click(object sender, EventArgs e)
+        {
+			String helpText = "This application is used to update the OASys Instance on your local virtual machine (VM)." + NL + NL;
+			helpText += " - Ensure that your local virtual machine is running" + NL + " - Ensure you are connected to the OASys Azure VPN" + NL + NL;
+			helpText += "This process will OVERWRITE your OASys application AND database so you will lose ANY data that you have created locally." + NL + NL;
+			helpText += "Click the 'Update VM' (RUN) button to begin the process. Once the process has started it should NOT be interrupted or data loss will occur.";
+			MessageBox.Show(helpText,"VMUpd : Capita");
+        }
+    }
+}
